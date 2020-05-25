f74984095e915ad81291e3df414a7f4361a3e187
haseokwoo <seokwooha@student.cofo.edu>
2020-05-25 17:11:49 -0500

    Circuit Practice using ArrayList Done.

--- CircuitPractice/src/Circuit.java
+++ CircuitPractice/src/Circuit.java
@@ -1,20 +1,21 @@
+import java.util.ArrayList;
 
 public class Circuit {
 
-	public double series(int input, double[] array) {
+	public double series(int input, ArrayList<Double> array) {
 		double value = 0;
 
-		for (int y = 0; y < array.length; y++) {
-			value += array[y];
+		for (int y = 0; y < array.size(); y++) {
+			value += array.get(y);
 		}
 		return value;
 	}
 
-	public double parallel(int input, double[] array) {
+	public double parallel(int input, ArrayList<Double> array) {
 		double value2 = 0;
 
-		for (int y = 0; y < array.length; y++) {
-			value2 += (array[y]);
+		for (int y = 0; y < array.size(); y++) {
+			value2 += array.get(y);
 		}
 		value2 = 1 / value2;
 		return value2;
--- CircuitPractice/src/Main.java
+++ CircuitPractice/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -19,32 +20,33 @@ public class Main {
 			System.out.println("You chose a series type of circuit.\n");
 			System.out.println("How many circuits do you have?\n");
 			userInput = keyboard.nextInt();
-
-			double[] array = new double[userInput];
+			
+			ArrayList<Double> list1 = new ArrayList<>();
 			double value;
-			for (int i = 0; i < array.length; i++) {
+			for (int i = 0; i < userInput; i++) {
 				System.out.println("Please give the value of " + (i + 1) + " circuit.");
 				value = keyboard.nextDouble();
-				array[i] = value;
+				list1.add(value);
 			}
 
 			System.out.printf("Total resistance of your series circuit is: %.2f" + " Ohms.",
-					c.series(userInput, array));
+					c.series(userInput, list1));
 		} else if (userChoice == 2) {
 			System.out.println("You chose a parallel type of circuit.\n");
 			System.out.println("How many circuits do you have?\n");
 			userInput = keyboard.nextInt();
 
-			double[] array2 = new double[userInput];
+			ArrayList<Double> list2 = new ArrayList<>();
 			double value2;
-			for (int i = 0; i < array2.length; i++) {
+			for (int i = 0; i < userInput; i++) {
 				System.out.println("Please give the value of " + (i + 1) + " circuit.");
 				value2 = keyboard.nextDouble();
-				array2[i] = 1 / value2;
+				value2 = 1 / value2;
+				list2.add(value2);
 			}
 
 			System.out.printf("Total resistance of your parallel circuit is: %.2f" + " Ohms.",
-					c.parallel(userInput, array2));
+					c.parallel(userInput, list2));
 		} else {
 			System.out.println("Invalid choice. Please choose either 1 or 2.");
 			main(args);
